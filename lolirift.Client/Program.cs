b52eeedcabed86482483906d6057456e22d8b230
Hydro844 <hydro844@cock.li>
2016-06-09 16:01:20 +0200

    client async reception of host packets implemented

--- lolirift.Client/Program.cs
+++ lolirift.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using lolirift.Client.Controllers;
+using lolirift.Client.Controllers.External;
 using lolirift.Client.Controllers.Internal;
 using Newtonsoft.Json;
 using System;
@@ -17,7 +18,6 @@ namespace lolirift.Client
         {
             var endPoint = args[0].Split(':');
             var tcp = new TcpClient();
-            var dict = new Dictionary<string, string>();
 
             var data = new DataStore();
             data.Tcp = tcp;
@@ -27,10 +27,49 @@ namespace lolirift.Client
                 new InHelloController(data)
             };
 
+            var exControllers = new Controller[]
+            {
+                new ExHelloController(data)
+            };
+
+            
             Console.WriteLine("Connecting to the host...");
             tcp.Connect(IPAddress.Parse(endPoint[0]), int.Parse(endPoint[1]));
             Console.WriteLine("Connecting successful!");
             Console.WriteLine();
+
+            Console.WriteLine("Starting to receive packets by host");
+            new Task(() =>
+            {
+                var net = tcp.GetStream();
+
+                var buffer = new byte[4096];
+                var length = net.Read(buffer, 0, buffer.Length);
+
+                if (length == 0)
+                    return;
+
+                var json = string.Empty;
+                var openBracketCount = buffer.Count(b => b == '{');
+                var closedBracketCount = buffer.Count(b => b == '}');
+                json += Encoding.UTF8.GetString(buffer, 0, length);
+
+                while (openBracketCount != closedBracketCount)
+                {
+                    length = net.Read(buffer, 0, buffer.Length);
+                    json += Encoding.UTF8.GetString(buffer, 0, length);
+                }
+
+                var dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+
+                try
+                {
+                    foreach (var controller in exControllers)
+                        if (controller.Executable(dict))
+                            controller.Execute(dict);
+                }
+                catch (Exception e) { Console.WriteLine("Executing controller went wrong! Failure Message: " + e.Message); }
+            }).Start();
 
             while (true)
             {
@@ -38,7 +77,7 @@ namespace lolirift.Client
                 Console.WriteLine("We recommend using \"hello\"");
                 var line = Console.ReadLine();
 
-                dict = new Dictionary<string, string>();
+                var dict = new Dictionary<string, string>();
 
                 dict.Add("controller", line.Split(' ')[0]);
                 dict.Add("args", line.Split(' ').Length > 1 ? (line.Substring(line.IndexOf(' ') + 1)) : string.Empty);
