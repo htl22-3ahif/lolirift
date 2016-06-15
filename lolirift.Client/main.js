f3c7094ae4f73bc01c06478599613d88a2d7222f
Hydro844 <hydro844@cock.li>
2016-06-15 16:24:27 +0200

    sliding added to client

--- lolirift.Client/main.js
+++ lolirift.Client/main.js
@@ -2,6 +2,9 @@ var ws = new WebSocket("ws://127.0.0.1:844");
 var isMouseDown = false;
 var posX = 0;
 var posY = 0;
+var gridX = 0;
+var gridY = 0;
+var scale = 1;
 
 var controllers = [
     function(j){
@@ -32,27 +35,41 @@ var controllers = [
 ws.onopen = function(){
     ws.send( '{ "controller": "map" }' );
     
-    console.log("defining events");
     
-    $( '#grid' ).mousedown(function(e){
-        console.log("mousedown");
+    $( 'html' ).mousedown(function(e){
         isMouseDown = true;
         posX = e.pageX;
-        poxY = e.pageY;
+        posY = e.pageY;
     });
     
-    $( '#grid' ).mouseup(function(){
-        console.log("mouseup");
+    $( 'html' ).mouseup(function(){
         isMouseDown = false;
     });
     
-    $( '#grid' ).mousemove(function(e){
+    $( 'html' ).mousemove(function(e){
         if (!isMouseDown)
             return;
-        
-        console.log(e.pageX + " " + e.pageY);
+		gridX += (posX - e.pageX) * (1/scale);
+		gridY += (posY - e.pageY) * (1/scale);
+        posX = e.pageX;
+        posY = e.pageY;
+		$('#grid').css({
+            'right': gridX,
+            'bottom': gridY
+		});
     });
-    console.log("event made");
+    
+    $ ( 'html' ).bind('mousewheel', function(e){
+        if(e.originalEvent.wheelDelta /120 > 0) {
+            scale += 0.1 * scale;
+        }
+        else{
+            scale -= 0.1 * scale;
+        }
+        $(this).css({
+            'transform': 'scale('+scale+')'
+        });
+    });
 };
 
 ws.onerror = function(error){
