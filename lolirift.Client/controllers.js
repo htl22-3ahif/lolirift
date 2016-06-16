cea8919b0070dbf871da10a66db47c8a4f11e211
Hydro844 <hydro844@cock.li>
2016-06-16 08:17:35 +0200

    forgot to add the other js datas like controllers and modals

---
+++ lolirift.Client/controllers.js
@@ -0,0 +1,37 @@
+var controllers = [
+    function(j){
+        if (j["controller"] != "map")
+            return;
+        
+        console.log ( "Map controller aktive" );
+        
+        var width = parseInt( j["width"] );
+        var height = parseInt( j["height"] );
+        
+        for (i = 0; i < width * height; i++){
+            var elem = $( '<div class="field"></div>' );
+            elem.css({
+                'width': (1/width)*100+'%',
+                'height': (1/height)*100+'%'
+            });
+            elem.attr('pos', i%width+'/'+Math.floor(i/width));
+            $('#grid').append(elem);
+        }
+        
+        $('#grid').css({
+            'width': width * 100+'px',
+            'height': height * 100+'px'
+        });
+    }
+    ,function(j){
+        if (j["controller"] != "see")
+            return;
+        
+        console.log("see controller aktive");
+        
+        j["seeable"].forEach(function(s){
+            var info = s["information"];
+            $ ( '.field[pos="'+s['x']+'/'+s['y']+'"]' ).html(info["unit"]+" by "+info["owner"]);
+        });
+    }
+];
---
+++ lolirift.Client/modals.js
@@ -0,0 +1,36 @@
+var modals = {
+    'build': `
+<div class="modal-content">
+  <div class="modal-title">Build</div>
+  <div class="row">
+    <div class="input-field col s12">
+      <input id="building-name" type="text" class="validate">
+      <label for="building-name">Name of building</label>
+    </div>
+  </div>
+  <div class="row">
+    <div class="input-field col s6">
+      <input id="position-x" type="text" class="validate">
+      <label for="position-x">Position X</label>
+    </div>
+    <div class="input-field col s6">
+      <input id="position-y" type="text" class="validate">
+      <label for="position-y">Position Y</label>
+    </div>
+    <a class="btn waves-effect waves-light" onclick="submit()">Submit
+      <i class="material-icons right">send</i>
+    </a>
+  </div> 
+  <script>
+  function submit(){
+      var name = $ ( '#building-name' ).val();
+      var x = $ ( '#position-x' ).val();
+      var y = $ ( '#position-y' ).val();
+      
+      console.log(JSON.stringify({"controller":"build","name": name,"x":x,"y":y}));
+      ws.send(JSON.stringify({"controller":"build","name": name,"x":x,"y":y}));
+  }
+  </script>
+</div>
+`
+};
